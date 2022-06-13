d4cd782ccd4b9092012f1fd565df92e94a6685d9
Jan Majerníček <arcanusvontenebris@gmail.com>
2022-06-13 20:24:00 +0200

    Change Section component, landing page and NewsItem + some styling

--- src/shared/components/Newsfeed/NewsItem.tsx
+++ src/shared/components/Newsfeed/NewsItem.tsx
@@ -2,10 +2,20 @@ import { NewsItemModel } from "../../models/news-item";
 import styles from "./NewsItem.module.css";
 
 const NewsItem = (props: NewsItemModel) => {
-  return <div className={`${styles["news-item"]} ${props.className}`}>
-    <h2>{props.title}</h2>
-    <h3>{props.leadParagraph}</h3>
-  </div>;
+  return (
+    <div
+      className={`${styles["news-item"]} ${props.className} ${
+        props.importance === 1
+          ? styles["news-item__one"]
+          : props.importance === 2
+          ? styles["news-item__two"]
+          : undefined
+      }`}
+    >
+      <h2>{props.title}</h2>
+      <p>{props.leadParagraph}</p>
+    </div>
+  );
 };
 
 export default NewsItem;
